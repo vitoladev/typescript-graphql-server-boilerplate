518c11734f7ef6eb78e53be78c953f36e2453641
Victor <asaptorvi@protonmail.com>
2021-09-05 18:48:39 -0300

    feat: add mercurius and altair with custom context

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,20 @@
 import fastify from 'fastify';
+import AltairFastify from 'altair-fastify-plugin';
+import mercurius from 'mercurius';
+import schema from './graphql';
+import buildContext from './graphql/context';
 
 const app = fastify({ logger: true });
+
+app.register(mercurius, {
+  schema,
+  context: buildContext,
+});
+
+app.register(AltairFastify, {
+  path: '/altair',
+  endpointURL: '/graphql',
+});
 
 const start = async () => {
   try {
